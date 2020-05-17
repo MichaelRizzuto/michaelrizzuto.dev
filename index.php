8bf295e7447e71d203cf4167782fa65af17a9ddb
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-05-17 03:28:34 -0400

    Update

--- index.php
+++ index.php
@@ -1,6 +1,47 @@
 <?php $pageName = 'Home';
 require_once('includes/header.php'); ?>
 <div class="home-container">
-	<p>Test</p>
+	<div class="home-hero">
+		
+	</div>
+	<div class="home-skills">
+		<h1>My Skills</h1>
+		<h2>LUA</h2>
+		<div class="skill-bg">
+			<div class="skill-actual" style="width: 100%;">
+				<p>100%</p>
+			</div>
+		</div>
+		<h2>PHP</h2>
+		<div class="skill-bg">
+			<div class="skill-actual" style="width: 80%;">
+				<p>80%</p>
+			</div>
+		</div>
+		<h2>Javascript/Node</h2>
+		<div class="skill-bg">
+			<div class="skill-actual" style="width: 95%;">
+				<p>95%</p>
+			</div>
+		</div>
+		<h2>C#</h2>
+		<div class="skill-bg">
+			<div class="skill-actual" style="width: 80%;">
+				<p>80%</p>
+			</div>
+		</div>
+		<h2>HTML</h2>
+		<div class="skill-bg">
+			<div class="skill-actual" style="width: 100%;">
+				<p>100%</p>
+			</div>
+		</div>
+		<h2>CSS</h2>
+		<div class="skill-bg">
+			<div class="skill-actual" style="width: 100%;">
+				<p>100%</p>
+			</div>
+		</div>
+	</div>
 </div>
 <?php require_once('includes/footer.php'); ?>
