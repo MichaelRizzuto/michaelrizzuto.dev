9938a38aa2907e34b88920e9428e39764631c6d0
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-05-18 20:01:19 -0400

    Initial

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,15 @@
 	</body>
 	<footer>
-		<h1>Copyright &copy; 2020 Michael Rizzuto. All Rights Reserved.</h1>
+		<div class="footer-left-container">
+			<h1>third sector gaming</h1>
+			<p>Made by <a href="https://primestudios.co" target="_blank">Prime Studios</a>.</p>
+			<p>Copyright &copy; 2020 Third Sector Gaming. All Rights Reserved.</p>
+		</div>
+		<div class="footer-right-container">
+			<ul>
+				<a href="#" target="_blank"><li><i class="fab fa-steam"></i></li></a>
+				<a href="#" target="_blank"><li><i class="fab fa-discord"></i></li></a>
+			</ul>
+		</div>
 	</footer>
 </html>
