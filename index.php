02a5896f8c5f3f8c11ee2e847384c63e829f3907
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-06-29 20:16:52 -0400

    Fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php $pageName = 'Portfolio';
 require_once('includes/header.php'); ?>
 <div class="content-container">
-	<div class="content-about">
+	<div class="about-container">
 		<h2>about</h2>
 		<h1>Creating <span id="typewriter"></span> that work</h1>
 		<script>
@@ -32,6 +32,10 @@ require_once('includes/header.php'); ?>
 			<img title="CSS" src="/assets/img/css.png" />
 			<img title="LUA" src="/assets/img/lua.png" />
 			<img title="NodeJS" src="/assets/img/nodejs.png" />
+		</div>
+		<h2>information about me</h2>
+		<div class="about-description">
+			<p>My name is <strong>Michael Rizzuto</strong>, I am a sixteen year old technology enthusiast based in Pennsylvania. I got my start in the gaming industry, where I still find myself spending some of my sparetime, but quickly evolved into a passion and desire to create my own games or contribute to others. During this time I also got introduced to the hosting industry where I found employment and a source of income. I spend my time nowadays enjoying my hobbies and doing freelance for others. My hobbies include gaming, as mentioned earlier, football, wrestling and overall I'm just a social person and enjoy talking with others.</p>
 		</div>
 	</div>
 </div>
