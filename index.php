fa7e8d097cc46103a8ff64d1773a60058319d49e
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-06-29 23:23:25 -0400

    Fix

--- index.php
+++ index.php
@@ -24,6 +24,10 @@ require_once('includes/header.php'); ?>
 			    .pause(1000)
 			    .go();
 		</script>
+		<h2>information about me</h2>
+		<div class="about-description">
+			<p>My name is <strong>Michael Rizzuto</strong>, I am a sixteen year old technology enthusiast based in Pennsylvania. I got my start in the gaming industry, where I still find myself spending some of my sparetime, but quickly evolved into a passion and desire to create my own games or contribute to others. During this time I also got introduced to the hosting industry where I found employment and a source of income. I spend my time nowadays enjoying my hobbies and doing freelance for others. My hobbies include gaming, as mentioned earlier, football, wrestling and overall I'm just a social person and enjoy talking with others.</p>
+		</div>
 		<h2>languages i work with</h2>
 		<div class="about-languages">
 			<img title="PHP" src="/assets/img/php.png" />
@@ -33,10 +37,6 @@ require_once('includes/header.php'); ?>
 			<img title="LUA" src="/assets/img/lua.png" />
 			<img title="C#" src="/assets/img/csharp.png" />
 			<img title="NodeJS" src="/assets/img/nodejs.png" />
-		</div>
-		<h2>information about me</h2>
-		<div class="about-description">
-			<p>My name is <strong>Michael Rizzuto</strong>, I am a sixteen year old technology enthusiast based in Pennsylvania. I got my start in the gaming industry, where I still find myself spending some of my sparetime, but quickly evolved into a passion and desire to create my own games or contribute to others. During this time I also got introduced to the hosting industry where I found employment and a source of income. I spend my time nowadays enjoying my hobbies and doing freelance for others. My hobbies include gaming, as mentioned earlier, football, wrestling and overall I'm just a social person and enjoy talking with others.</p>
 		</div>
 	</div>
 	<div class="projects-container">
