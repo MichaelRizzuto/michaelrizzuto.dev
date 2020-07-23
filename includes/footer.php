446375ecb4438ce2f067385f5a2b5e22a569a06c
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-07-23 16:00:40 -0400

    fix

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,5 @@
 	</body>
+	<footer>
+		<h1>&copy; <script>document.write(new Date().getFullYear())</script> MICHAEL RIZZUTO. ALL RIGHTS RESERVED. ALL TRADEMARKS AND REGISTERED TRADEMARKS ARE THE PROPERTY OF THEIR RESPECTIVE OWNERS.</h1>
+	</footer>
 </html>
