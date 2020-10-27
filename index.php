ca7d1cb0b8302e0817e56503d0f0e6135844fade
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-10-27 04:05:09 -0400

    Update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once('includes/header.php'); ?>
 		<div class="about-description">
 			<p>My name is <strong>Michael Rizzuto</strong>, I am a sixteen year old technology enthusiast based in Pennsylvania. I got my start in the gaming industry, where I still find myself spending some of my sparetime, but quickly evolved into a passion and desire to create my own games or contribute to others. During this time I also got introduced to the hosting industry where I found employment and a source of income. I spend my time nowadays enjoying my hobbies and doing freelance for others. My hobbies include gaming, as mentioned earlier, football, wrestling and overall I'm just a social person and enjoy talking with others.</p>
 		</div>
-		<h2>stacks i work with</h2>
+		<h2>what i work with</h2>
 		<div class="about-languages">
 			<img title="PHP" src="/assets/img/php.png" />
 			<img title="Javascript" src="/assets/img/javascript.png" />
@@ -17,6 +17,7 @@ require_once('includes/header.php'); ?>
 			<img title="LUA" src="/assets/img/lua.png" />
 			<img title="C#" src="/assets/img/csharp.png" />
 			<img title="NodeJS" src="/assets/img/nodejs.png" />
+			<img title="React JS" src="/assets/img/react.png" />
 			<img title="MySQL" src="/assets/img/mysql.png" />
 		</div>
 	</div>
@@ -29,6 +30,15 @@ require_once('includes/header.php'); ?>
 						<img src="/assets/img/primestudios.png" />
 						<h1>Prime Studios</h1>
 						<p>Prime Studios is a freelance company focused on building powerful websites for our clients.</p>
+					</div>
+				</a>
+			</div>
+			<div class="project-individual">
+				<a href="https://valiantservers.net" target="_blank">
+					<div class="project-link">
+						<img src="/assets/img/valiant.png" />
+						<h1>Valaint Servers</h1>
+						<p>A custom tailored Garry's Mod experience based on City Roleplay.</p>
 					</div>
 				</a>
 			</div>
