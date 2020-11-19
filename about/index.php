5a132bf78131f960ee265428fb667d88dea13db3
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-11-19 12:48:53 -0500

    About page update

--- about/index.php
+++ about/index.php
@@ -15,6 +15,18 @@ require_once('../includes/header.php'); ?>
 			<div class="about-summary">
 				<p>My name is Michael Rizzuto, I am a sixteen year old technology enthusiast based in Pennsylvania. I got my start in the gaming industry, where I still find myself spending some of my sparetime, but quickly evolved into a passion and desire to create my own games or contribute to others. During this time I also got introduced to the hosting industry where I found employment and a source of income. I spend my time nowadays enjoying my hobbies and doing freelance for others. My hobbies include gaming, as mentioned earlier, football, wrestling and overall I'm just a social person and enjoy talking with others.</p>
 			</div>
+			<h1>What I work with</h1>
+			<div class="about-languages">
+				<img title="PHP" src="/assets/img/php.png" />
+				<img title="Javascript" src="/assets/img/javascript.png" />
+				<img title="HTML" src="/assets/img/html.png" />
+				<img title="CSS" src="/assets/img/css.png" />
+				<img title="LUA" src="/assets/img/lua.png" />
+				<img title="C#" src="/assets/img/csharp.png" />
+				<img title="NodeJS" src="/assets/img/nodejs.png" />
+				<img title="React JS" src="/assets/img/react.png" />
+				<img title="MySQL" src="/assets/img/mysql.png" />
+			</div>
 			<h1>Timeline</h1>
 			<div class="about-timeline-benchmarks">
 				<div class="about-timeline-line">
