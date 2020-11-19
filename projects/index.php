34c745d450ea82df43f0246802485f026a6d6b41
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-11-19 17:56:09 -0500

    Projects

--- projects/index.php
+++ projects/index.php
@@ -9,7 +9,43 @@ require_once('../includes/header.php'); ?>
 	</div>
 	<div class="page-divider">
 	</div>
-	<div class="about-container">
+	<div class="projects-container">
+		<div class="project-container">
+			<img src="/assets/img/primestudios.png" />
+			<h1>Prime Studios</h1>
+			<h2>Company Director | 2019-Present</h2>
+			<p>Prime Studios is a freelance company focused on building powerful websites for our clients.</p>
+		</div>
+		<div class="project-container project-padding">
+			<img src="/assets/img/valiant.png" />
+			<h1>Valiant Servers</h1>
+			<h2>Root | 2020-Present</h2>
+			<p>A custom tailored Garry's Mod experience based on City Roleplay.</p>
+		</div>
+		<div class="project-container project-padding">
+			<img src="/assets/img/padojrp.png" />
+			<h1>PADOJRP</h1>
+			<h2>Director | 2019-2020</h2>
+			<p>A custom tailored Garry's Mod experience based on City Roleplay.</p>
+		</div>
+		<div class="project-container project-padding">
+			<img src="/assets/img/encryptedlaser.png" />
+			<h1>Encrypted Laser</h1>
+			<h2>Systems Administrator | 2017-Present</h2>
+			<p>Encrypted Laser is a company that is mainly engaged in the hosting industry.</p>
+		</div>
+		<div class="project-container project-padding">
+			<img src="/assets/img/thiradus.png" />
+			<h1>Thiradus</h1>
+			<h2>C.I.O | 2015-Present</h2>
+			<p>Thiradus is a software development company targetted at creating products for gaming communities.</p>
+		</div>
+		<div class="project-container project-padding">
+			<img src="/assets/img/atlas.png" />
+			<h1>Atlas Uprising</h1>
+			<h2>Developer | 2020-Present</h2>
+			<p>Atlas Uprising is a Multi-Game community that has acomplished a great amount of success in Garry's Mod and Rust.</p>
+		</div>
 	</div>
 </div>
 <?php require_once('../includes/footer.php'); ?>
